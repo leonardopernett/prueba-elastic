218e4a6ab14a89fdad3b4ac8fdff4734325b2068
leonardo pernett <lnard.dev@gmail.com>
2022-05-23 10:30:19 -0500

    config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 const express = require('express')
-const AWS = require('aws-sdk');
-const  path  = require('path');
-const app = express()
-const fs = require('fs')
+const AWS     = require('aws-sdk');
+const path    = require('path');
+const fs      = require('fs')
 
+const app     = express()
 
 const s3 = new AWS.S3({
-    secretAccessKey:'e29j/D1s2V5VZPuti8pHMfH7Yucq3HvabMDHjwOS',
-    accessKeyId:'AKIAVQGYG2RXPN3OU3WQ', 
-    region: 'us-east-1',
-    sslEnabled: false
+  region:'us-east-1',
+  sslEnabled:false
 })
-
-var filePath = path.resolve('./public/uploads/img.jpg');
 
 app.get('/', async (req,res) => {
 
-    var params = {
-      Bucket: 'aws-nik-mx-bk',
-      Body : fs.createReadStream(filePath),
-      Key : "folder/"+Date.now()+"_"+path.basename(filePath) 
+    let params = {
+        Bucket: 'aws-nik-mx-bk',
+        Body : fs.createReadStream(path.resolve('./public/uploads/img.jpg')),
+        Key : "folder/"+Date.now()+"_"+path.basename('./public/uploads/img.jpg'),
+        ACL:'public-read'
     };
 
     try {
@@ -31,6 +28,8 @@ app.get('/', async (req,res) => {
    
     res.json({ data:'image uploaded' }) 
  })
+
+
  app.use(express.static(path.resolve('./public')))
 
  app.listen(3000,() => console.log('server on port 3000'))
